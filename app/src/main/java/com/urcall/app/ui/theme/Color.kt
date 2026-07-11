98e2fa9ccbd94892e4ef6af2017d8c13c0ecbb97
edisondayags-web <edisondayags@gmail.com>
2026-07-11 23:08:27 +0800

    Update Color.kt

--- app/src/main/java/com/urcall/app/ui/theme/Color.kt
+++ app/src/main/java/com/urcall/app/ui/theme/Color.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 val UrBlack = Color(0xFF0A0002)
 val UrCard = Color(0x33FFFFFF)
-val UrNeon = Color(0xFFD4FF3D)
+val UrNeon = Color(0xFFA3D900)
 val UrPink = Color(0xFFE6007A)
 val UrPinkGlow = Color(0xFFB3004F)
 val UrTextWhite = Color(0xFFF5F5F5)
